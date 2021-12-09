35cc276a958bb4ab2df0ffd85e83731f721a105c
ehsan <ehsan2003.2003.382@gmail.com>
2021-12-09 18:54:33 +0330

    adds Validatable trait to utils

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,9 +2,12 @@ pub use auth_payload::AuthPayload;
 pub use crypto_service::CryptoService;
 pub use interactor::Interactor;
 pub use random_service::RandomService;
+pub use validatable::Validatable;
 
 mod random_service;
 mod crypto_service;
 mod interactor;
 mod auth_payload;
+mod validatable;
+
 
