9dcba3a613c681dcc7f46501631d4ccb634c0a89
ehsan <ehsan2003.2003.382@gmail.com>
2021-12-13 15:12:17 +0330

    adds a separated ValidationError struct to the errors module

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -16,3 +16,19 @@ pub type UnknownException = Box<dyn std::error::Error + Send + Sync>;
 
 pub type ApplicationResult<T> = Result<T, ApplicationException>;
 pub type UnknownResult<T> = Result<T, UnknownException>;
+
+pub struct ValidationError {
+    pub key: String,
+    pub value: String,
+    pub message: String,
+}
+
+impl From<ValidationError> for ApplicationException {
+    fn from(error: ValidationError) -> Self {
+        ApplicationException::ValidationException {
+            key: error.key,
+            value: error.value,
+            message: error.message,
+        }
+    }
+}
