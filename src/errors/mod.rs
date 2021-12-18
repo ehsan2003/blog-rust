0d37fa1a4bf11f8f219d2817f63e282d97858ff5
ehsan <ehsan2003.2003.382@gmail.com>
2021-12-18 14:33:21 +0330

    implements Error trait for ApplicationError type

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -1,4 +1,7 @@
+use std::error::Error;
+use std::fmt::Formatter;
 
+use ApplicationException::*;
 
 pub mod auth_payload_id_not_found;
 pub mod validation;
@@ -18,6 +21,20 @@ pub enum ApplicationException {
     },
     InternalException(UnknownException),
     ForBiddenException(String),
+}
+
+impl std::fmt::Display for ApplicationException {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        format!("{:?}", self).fmt(f)
+    }
+}
+impl std::error::Error for ApplicationException {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            InternalException(internal) => Some(internal.as_ref()),
+            _ => None,
+        }
+    }
 }
 
 pub type UnknownException = Box<dyn std::error::Error + Send + Sync>;
