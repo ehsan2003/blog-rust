7788f6d51949943bcf7a651c4f6291f4ae038006
ehsan <ehsan2003.2003.382@gmail.com>
2021-12-18 16:05:31 +0330

    adds a method to auth_with_password_validator.rs for simplicity of usage

--- src/utils/traits/auth_with_password_validator.rs
+++ src/utils/traits/auth_with_password_validator.rs
@@ -1,7 +1,19 @@
-use crate::errors::UnknownResult;
+use ApplicationException::BadRequestException;
+
+use crate::errors::{ApplicationException, ApplicationResult, UnknownResult};
 use crate::utils::AuthPayload;
 
 #[async_trait::async_trait]
 pub trait AuthWithPasswordValidator {
     async fn validate(&self, auth: &(dyn AuthPayload), password: &str) -> UnknownResult<bool>;
+    async fn validate_or_fail(
+        &self,
+        auth: &(dyn AuthPayload),
+        password: &str,
+    ) -> ApplicationResult<()> {
+        match self.validate(auth, password).await? {
+            true => Ok(()),
+            false => Err(BadRequestException("invalid password".into())),
+        }
+    }
 }
